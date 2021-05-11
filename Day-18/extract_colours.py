b9d2a4741b3e86d692f238245c1d12b992d0726c
Mundir Ali <mundirali2003@gmail.com>
2021-05-11 15:56:40 +0100

    Changed a comment

--- Day-18/extract_colours.py
+++ Day-18/extract_colours.py
@@ -5,5 +5,4 @@ for i in range(len(colors)):
     r_g_b = (colors[i].rgb.r, colors[i].rgb.g, colors[i].rgb.b)
     colours.append(r_g_b)
 
-# What I did to extract the colours from the image and
-# append to a list
+# What I did to extract the colours from the image and append to a list
