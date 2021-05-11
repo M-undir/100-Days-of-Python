bb8415d98844f8d02dd7407027b934a941a79773
Mundir Ali <mundirali2003@gmail.com>
2021-05-11 15:53:46 +0100

    Finished Day-18

---
+++ Day-18/extract_colours.py
@@ -0,0 +1,6 @@
+# import colorgram
+# colors = colorgram.extract('image.jpg', 30)
+# colours = []
+# for i in range(len(colors)):
+#     r_g_b = (colors[i].rgb.r, colors[i].rgb.g, colors[i].rgb.b)
+#     colours.append(r_g_b)
--- Day-18/main.py
+++ Day-18/main.py
@@ -1,4 +1,31 @@
-import colorgram
+import random
+import turtle
+from colours import colour_list
+from turtle import Turtle, Screen
+turtle.colormode(255)
+
+hirst_painting = Turtle()
+# x and y-axis reach up to 10
+# dots are 20 in size and spaced by 50m (.fd(50))
+
+hirst_painting.ht()  # hides the turtle
+# hirst_painting.pensize(20)
+hirst_painting.penup()
+columns_to_complete = 10
+y_cor = -200
+hirst_painting.goto(x=-200, y=y_cor)
+
+while columns_to_complete > 0:
+    for rows in range(10):
+        hirst_painting.dot(20, random.choice(colour_list))
+        hirst_painting.fd(50)
+    y_cor += 50
+    hirst_painting.goto(x=-200, y=y_cor)
+    columns_to_complete -= 1
 
 
-colorgram.extract
+screen = Screen()
+screen.exitonclick()
+
+
+
