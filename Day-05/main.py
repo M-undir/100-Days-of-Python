f081a5a4cc4c8b12a6c4c35a734eefdcc54f8a02
Mundir Ali <mundirali2003@gmail.com>
2021-08-10 13:17:41 +0100

    Finished Day-05

---
+++ Day-05/main.py
@@ -0,0 +1,21 @@
+import random
+from data import *
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+password = ""
+for letter in range(nr_letters):
+    password += random.choice(letters)
+
+for symbol in range(nr_symbols):
+    password += random.choice(symbols)
+
+for number in range(nr_numbers):
+    password += random.choice(numbers)
+
+# print(password)
+random_password = ''.join(random.sample(password, len(password)))
+print(random_password)
