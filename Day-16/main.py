a31ad3d6e89ad9f99940f4fabde7ec8311092f96
Mundir Ali <mundirali2003@gmail.com>
2021-08-22 09:39:50 +0100

    Finished Day-16

---
+++ Day-16/main.py
@@ -0,0 +1,22 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+ordering = True
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money = MoneyMachine()
+
+while ordering:
+    choice = input(f"What would you like? ({menu.get_items()}): ").lower()
+    if choice == "off":
+        break
+    elif choice == 'report':
+        coffee_maker.report()
+        money.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink):
+            if money.make_payment(drink.cost):
+                # print('yes')
+                coffee_maker.make_coffee(drink)
---
+++ Day-16/pre-work.py
@@ -0,0 +1,47 @@
+# from turtle import Turtle, Screen
+#
+# screen = Screen()
+# me = Turtle()
+#
+#
+# # headings = [0, 90, 180, 270]
+# # me.fd(10)
+# # for i in headings:
+# #     me.seth(i)
+# me.color('white')
+# me.fd(100)
+# screen.bgcolor('black')
+#
+#
+#
+#
+# screen.exitonclick()
+
+
+# from prettytable import PrettyTable
+#
+# table = PrettyTable()
+#
+# table.field_names = ["Character", "Country", "Class"]
+# # table.add_row(["Grid", "South Korea", "Legendary Blacksmith"])
+# # table.add_row(["Braham Eswald", "???", "Legendary Magician"])
+# table.add_rows(
+#     [
+#         ["Grid", "South Korea", "Legendary Blacksmith, Duke of Virtue"],
+#         ["Braham Eswald", "???", "Legendary Magician"],
+#         ["Jishuka", "Brazil", "Bow Saint"],
+#         ["Kraugel", "Russia(South Korea?)", "Sword Saint"]
+#
+#
+#
+#
+#     ]
+#
+#
+# )
+# table.add_column("Game",
+#                  ["Satisfy", "NPC (in Satisfy)", "Satisfy", "Satisfy"]
+#                  )
+# table.align["Class"] = "l"
+# print(table)
+# # print(table.get_string(fields=["Country"]))
